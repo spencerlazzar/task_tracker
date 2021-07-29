ac6c44229d9cf83a8446eef8fb325817f3373560
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-07-28 17:40:22 -0700

    Task Component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,12 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Grocery Shopping',
-    day: 'March 3rd at 2:30pm',
-    reminder: false,
-  },
-];
-const Tasks = () => {
-  return <div></div>;
+import Task from './Task';
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
