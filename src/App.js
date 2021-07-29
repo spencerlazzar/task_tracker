a3ca3e01386aef3365037614063cda3ca6cacdf8
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-07-28 18:08:31 -0700

    No Task Message

--- src/App.js
+++ src/App.js
@@ -23,10 +23,20 @@ function App() {
       reminder: false,
     },
   ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   );
 }
