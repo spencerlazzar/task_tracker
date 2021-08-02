863ba5dacec3538c6b70e97ddaaf3c9f9cb14748
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-08-02 16:39:44 -0700

    Form Submission

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -42,7 +49,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
